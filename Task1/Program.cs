e3b4a3e31a205f7082950cd8fdc3b88b9a34a783
dnskakun <dimon_sdn@list.ru>
2023-01-18 23:38:45 +0300

    Решение задачи №1

---
+++ Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 34. Задайтие массив заполненный случайными положительными трехзначными числами.
+// Напишите программу, которая покажет количество четных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+
+int size = new Random().Next(5,20);
+int[] array = FillArray(size, 100, 1000);
+int result = CheckArray(array);
+
+Console.WriteLine($"[{String.Join(", ", array)}] -> {result}");
+
+
+//Функции, используемые в программе
+int[] FillArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    var rnd = new Random();
+    for (int i = 0; i < size; i++) res[i] = rnd.Next(minValue, maxValue);
+    return res;
+}
+
+
+int CheckArray(int[] arr)
+{
+    int size = arr.Length;
+    int res = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] % 2 == 0) res += 1;
+    }
+    return res;
+}
